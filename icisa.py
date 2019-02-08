4bfac5eac268acd865e1bdd2345bb919198fbc51
EnricoBignotti <45388135+EnricoBignotti@users.noreply.github.com>
2019-02-08 18:57:57 +0100

    Added comments and ported in python3

--- icisa.py
+++ icisa.py
@@ -8,15 +8,17 @@ from TurtleOWL import *
 import operator
 #extracts the column from its coordinates
 def sortColumn(left):
-    return int(left)/433
+    return int(left)//433
+#checks if colums 0 or 1, 433 is based on pdf and checking how many pixels con l' applicazione crosshair da pdf
 
 #sorts the lines within the page
 def sortLines(page):
     lineList = list(page)
     lineList.sort(key=lambda x: (sortColumn(x.attributes['left'].value), int(x.attributes['top'].value)))
     return lineList
-
-#define a dedicated function for finding synonyms
+#difatto xml ha un tag che si chiama text. sort però non volgio l'alfabetico, voglio sortarli per attributi. key è la chiave
+#define a dedicated function for finding synonyms si basa sui tag left e top per iniziare, primo valore il numero della colonna e secondo valore con la distaza dall'alto 
+#classe element, attributes è un attr, name, local name, prefix è int perché è stored in string sorta il primo elemeto e poi il secondo indice
 def find_synonim(ontology, name):
     cleaned_name = name.upper().replace('.', '').strip()
     if not ontology.has_key(cleaned_name):
@@ -25,21 +27,26 @@ def find_synonim(ontology, name):
             synonims = []
             for par in range(1, len(segments)):
                 synonims = synonims + segments[par].replace(')', '').split(',')
+                #crea una lista di liste di tutte le parole in mezzo  alle parentesij
             #this is to remove the cases of synonyms that in the description are within parentheses
             for s in synonims:
                 if ontology.has_key(s.strip()):
                     return s.strip()
         out = re.sub(r'\([^)]*\)', '', cleaned_name).replace('  ',' ')
+        #range di caratteri tutti quelli non ) cancello tutti i caratteri tra le due parentesi
         if ontology.has_key(out):
             return out
         else:
             return difflib.get_close_matches(cleaned_name, ontology.keys(),1,0)[0]
+        #return only one not 3, il cutoff è la possibilità  ma non scartrlo perché deve esserci una roba quantomeno simile, se non la trovi
+        #prendi la cosa più simile
     else:
         return cleaned_name
 
 #parse the xml
 xmldoc = minidom.parse('icisa.xml')
 pages = xmldoc.getElementsByTagName('page')
+#iterable non è una lista
 #define the owl ontology
 icisa = TurtleOWL("http://www.semanticweb.org/it.enricobignotti/ICISA")
 
@@ -54,6 +61,8 @@ for index, page in enumerate(pages):
         for line in sortLines(page.getElementsByTagName('text')):
             if line.attributes['font'].value == '10' and line.attributes['height'].value == '17':
                 superclass = line.firstChild.firstChild.nodeValue
+                #<text top="305" left="53" width="136" height="17" font="10"><b>Insurable debtors</b></text> primo first prende b secondo il testo che poi prende nodevalue
+                #reach all the the way to text
                 #create the superclass removing spaces and putting uppercase and then create the rdfs:label to name the class
                 icisa.add_class(superclass.strip().upper())
                 icisa.add_annotation(superclass.strip().upper(), "rdfs:label", superclass, lang="en")
@@ -68,17 +77,21 @@ for index, page in enumerate(pages):
 
         #for each line, I check its position and depending on whether it is, I add it as a comment, specifying the language or as a lable of the class 
         for line in sortLines(page.getElementsByTagName('text')):
-            if line.attributes['font'].value == '10' and line.attributes['height'].value == '17' and className != "":
-                icisa.add_annotation(className, "rdfs:comment", message, lang='de')
-                message = ""
             if line.attributes['font'].value == '10' and line.attributes['height'].value == '17':
                 className = find_synonim(icisa.ontology, line.firstChild.firstChild.nodeValue)
+            if line.attributes['font'].value in ['11', '15'] and line.attributes['height'].value == '18':
+                message += line.firstChild.nodeValue
             if line.attributes['font'].value == '14' and line.attributes['height'].value == '17' and className != "":
                 icisa.add_annotation(className, 'rdfs:label', line.firstChild.firstChild.nodeValue, 'de')
                 icisa.add_annotation(className, "rdfs:comment", message, lang='en')
                 message = ""
-            if line.attributes['font'].value in ['11', '15'] and line.attributes['height'].value == '18':
-                message += line.firstChild.nodeValue
+                #guardo attraverso il testo il rpimo che trovo con valore è in en oppure in de
+            if line.attributes['font'].value == '10' and line.attributes['height'].value == '17' and className != "":
+                icisa.add_annotation(className, "rdfs:comment", message, lang='de')
+                message = ""
+                #evitato elif per non essere esclusivi, agiscono tutti insieme 3 e 4 uno per linea, se entri in uno
+           
+            
 
-print icisa.to_turtle()
+print(icisa.to_turtle())
 
